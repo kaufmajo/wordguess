657bbeb940a0ea2d33c71b7533c6a19c858e26af
kaufmajo <joachim_kaufmann@bluewin.ch>
2025-02-20 17:18:03 +0100

    ...

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,34 +1,47 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import Letter from './Letter.vue'
-import Placeholder from './Placeholder.vue'
+import Blank from './Blank.vue'
 
 const alphabet = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
-const word = 'hello';
-const guesses = ref(Array.from({ length: word.length }, () => ''));
+const word = ref(Array.from('hello'));
+const guesses = ref(Array.from({ length: word.value.length }, () => ''));
 const maxGuesses = 6;
+
+function handleGuess(char) {
+  word.value.map((letter, index) => {
+    if (letter === char) {
+      guesses.value[index] = char;
+    }
+  })
+
+  if (guesses.value.join('') === word.value.join('')) {
+    alert('You win!');
+  }
+};
 
 </script>
 
 <template>
-  
-  <div class="placeholder">
-    <Placeholder v-for="char in guesses" :key="char" :char="char" />
+
+  <div class="blank">
+    <Blank v-for="char in guesses" :key="char" :char="char" />
   </div>
-  
+
   <div class="keyboard">
-    <Letter v-for="char in alphabet" :key="char" :char="char" />
+    <Letter v-for="char in alphabet" :key="char" :char="char" @guess="handleGuess" />
   </div>
 
 </template>
 
 <style scoped>
-.placeholder {
+.blank {
   display: flex;
   flex-wrap: wrap;
   gap: 1rem;
   margin-bottom: 120px;
 }
+
 .keyboard {
   display: flex;
   flex-wrap: wrap;
